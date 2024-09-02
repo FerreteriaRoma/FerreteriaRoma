90ce031d5ff2867f9902f124e7a433275ffb07bb
JulianP <julprogram131@gmail.com>
2024-09-02 00:49:30 -0500

    Intento webhook3

--- pages/api/confirmation.js
+++ pages/api/confirmation.js
@@ -1,18 +1,21 @@
+// pages/api/confirmation.js
 import { NextResponse } from 'next/server';
+import crypto from 'crypto';
 
 export async function POST(req) {
   const { x_ref_payco, x_transaction_id, x_amount, x_currency_code, x_signature } = await req.json();
   console.log('Datos recibidos en la API:', { x_ref_payco, x_transaction_id, x_amount, x_currency_code, x_signature });
 
-  const p_cust_id_cliente = '1493107'; // Reemplaza con tu ID de cliente
-  const p_key = 'b7388587d565fb2e7df8c0cf79bf0cd852613208'; // Reemplaza con tu clave
+  // Reemplaza con tus claves de ePayco
+  const p_cust_id_cliente = '1493107'; // ID de cliente
+  const p_key = 'b7388587d565fb2e7df8c0cf79bf0cd852613208'; // Clave
 
   // Generar la firma esperada
   const signature = hash('sha256', `${p_cust_id_cliente}^${p_key}^${x_ref_payco}^${x_transaction_id}^${x_amount}^${x_currency_code}`);
-  console.log('Firma generada:', signature);
 
   // Validar firma
   if (x_signature === signature) {
+    // Aquí puedes hacer la lógica para guardar la transacción en tu base de datos
     return NextResponse.json({ status: 'success', message: 'Transacción confirmada' });
   } else {
     return NextResponse.json({ status: 'error', message: 'Firma no válida' });
@@ -21,6 +24,5 @@ export async function POST(req) {
 
 // Función de hash simple
 function hash(algorithm, data) {
-  const crypto = require('crypto');
   return crypto.createHash(algorithm).update(data).digest('hex');
 }
